60e402532ace3c21daa35ea19f99bd00f52ce772
rmiller <rmiller003@gmail.com>
2021-02-24 15:15:40 -0500

    Stage 1

--- agendax/app.py
+++ agendax/app.py
@@ -1,13 +1,14 @@
 # Agenda-X is a ToDo App built with Python 3.8, Flask and SQLDB
 # This app was programmed and designed by Robert Miller
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('base.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
