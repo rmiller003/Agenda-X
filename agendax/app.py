18997c86a49277bf119ce36940a5a40f38efda40
rmiller <rmiller003@gmail.com>
2021-03-01 11:32:41 -0500

    Complete

--- agendax/app.py
+++ agendax/app.py
@@ -1,7 +1,8 @@
 # Agenda-X is a ToDo App built with Python 3.8, Flask and SQLAlchemy
+# HTML Style sheets from semantic-ui
 # This app was programmed and designed by Robert Miller
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Task(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
@@ -18,10 +19,35 @@ class Task(db.Model):
 @app.route('/')
 def index():
     # show all tasks
-    task_list = Task.query.all()
-    return render_template('base.html', task_list=task_list)
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
+@app.route("/add", methods=["POST"])
+def add():
+    # add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # add new item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # add new item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
+
